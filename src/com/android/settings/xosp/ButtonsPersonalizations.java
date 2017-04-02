e2748c4648d0fc67ab56e165c9acf992653838ac
Carlo Savignano <carlosavignano@aospa.co>
2017-04-02 21:23:38 +0200

    Dynamic Navbar (2/2)
    Signed-off-by: Calin <nilac8991@gmail.com>

--- src/com/android/settings/xosp/ButtonsPersonalizations.java
+++ src/com/android/settings/xosp/ButtonsPersonalizations.java
@@ -16,53 +16,69 @@
 
 package com.android.settings.xosp;
 
-import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.Handler;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
 import android.util.Log;
-import android.view.IWindowManager;
-import android.view.KeyCharacterMap;
-import android.view.KeyEvent;
-import android.view.WindowManagerGlobal;
+import android.text.TextUtils;
 
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-import com.android.internal.logging.MetricsProto.MetricsEvent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import static android.provider.Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 //import com.android.internal.util.xosp.RebornUtils;
 
 public class ButtonsPersonalizations extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+        OnPreferenceChangeListener {
+
+    private static final String KEY_NAVIGATION_BAR = "navigation_bar";
+
+
+    private SwitchPreference mNavigationBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.xosp_buttons_cat);
+
+        final Resources res = getActivity().getResources();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mDeviceHardwareKeys = res.getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        /* Navigation Bar */
+        mNavigationBar = (SwitchPreference) findPreference(KEY_NAVIGATION_BAR);
+        if (mNavigationBar != null) {
+            if (mDeviceHardwareKeys != 0) {
+                mNavigationBar.setOnPreferenceChangeListener(this);
+            } else {
+                mNavigationBar = null;
+                removePreference(KEY_NAVIGATION_BAR);
+            }
+        }
     }
 
 
@@ -76,8 +92,82 @@ public class ButtonsPersonalizations extends SettingsPreferenceFragment implemen
         return MetricsEvent.XOSP;
     }
 
+    private boolean handleOnPreferenceTreeClick(Preference preference) {
+        if (preference != null && preference == mNavigationBar) {
+            mNavigationBar.setEnabled(false);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mNavigationBar.setEnabled(true);
+                }
+            }, 1000);
+            return true;
+        }
+        return false;
+    }
+
+    private String getSystemPreferenceString(Preference preference) {
+        if (preference == null) {
+            return EMPTY_STRING;
+        } else if (preference == mNavigationBar) {
+            return Settings.System.NAVIGATION_BAR_ENABLED;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        reload();
+    }
+
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+        final boolean handled = handleOnPreferenceChange(preference, newValue);
+        if (handled) {
+            reload();
+        }
+        return handled;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        final boolean handled = handleOnPreferenceTreeClick(preference);
+        // return super.onPreferenceTreeClick(preferenceScreen, preference);
+        return handled;
+    }
+
+    private void reload() {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final Resources res = getActivity().getResources();
+
+        final boolean defaultToNavigationBar = res.getBoolean(com.android.internal.R.bool.config_defaultToNavigationBar);
+        final boolean navigationBarEnabled = Settings.System.getIntForUser(resolver,
+                Settings.System.NAVIGATION_BAR_ENABLED, defaultToNavigationBar ? 1 : 0, UserHandle.USER_CURRENT) != 0;
+
+        if (mNavigationBar != null) {
+            mNavigationBar.setChecked(navigationBarEnabled);
+        }
+    }
+
+    private boolean handleOnPreferenceChange(Preference preference, Object newValue) {
+        final String setting = getSystemPreferenceString(preference);
+
+        if (TextUtils.isEmpty(setting)) {
+            // No system setting.
+            return false;
+        }
+
+        if (preference != null && preference instanceof SwitchPreference) {
+            boolean state = false;
+            if (newValue instanceof Boolean) {
+                state = (Boolean) newValue;
+            } else if (newValue instanceof String) {
+                state = Integer.valueOf((String) newValue) != 0;
+            }
+            Settings.System.putIntForUser(getContentResolver(), setting, state ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+        }
+
+        return true;
     }
 }
